03eabdf42360a77ac7ffc1ca52322138eff424a3
MuhammadFaizan42 <m.faizan42.mf@gmail.com>
2024-06-14 01:12:01 +0500

    sirius -erp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+import Dashboard from './pages/Home';
+import Header from './component';
+import "bootstrap/dist/css/bootstrap.min.css"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Header/>
+      <Routes>
+        <Route path="/" exact={true} element={<Dashboard/>} />
+        <Route path="/dashboard" exact={true} element={<Dashboard/>} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
