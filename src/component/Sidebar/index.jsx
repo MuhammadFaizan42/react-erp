00f1ac7e695359bbb0086c931b4d0336431083ec
MuhammadFaizan42 <m.faizan42.mf@gmail.com>
2024-06-28 01:39:23 +0500

    submenu design

--- src/component/Sidebar/index.jsx
+++ src/component/Sidebar/index.jsx
@@ -10,6 +10,7 @@ import { MdOutlineInventory2 } from "react-icons/md";
 import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
 import { TbReportSearch } from "react-icons/tb";
 import { IoSettingsOutline } from "react-icons/io5";
+import { Link } from 'react-router-dom';
 <IoSettingsOutline />
 
 
@@ -23,67 +24,95 @@ const Sidebar = () => {
     <div className="sidebar">
         <ul>
             <li>
-                <Button className='w-100'>
-                    <span className='icon'><RxDashboard /></span>
-                    Dashboard
-                    <span className='arrow'><FaAngleRight /></span>
-                </Button>
+                <Link to="/">
+                        <Button className='w-100'>
+                            <span className='icon'><RxDashboard /></span>
+                            Dashboard
+                            <span className='arrow'><FaAngleRight /></span>
+                        </Button>
+                </Link>
             </li>
             <li>
-                <Button className='w-100'>
-                    <span className='icon'><BiPurchaseTagAlt /></span>
-                    Purchase
-                    <span className='arrow'><FaAngleRight /></span>
-                </Button>
+                
+                        <Button className='w-100'>
+                            <span className='icon'><BiPurchaseTagAlt /></span>
+                            Purchase
+                            <span className='arrow'><FaAngleRight /></span>
+                        </Button>
+                        <div className='submenuWrapper'>
+                            <ul className='submenu'>
+                                <li><Link to="#">Purchase Order</Link></li>
+                                <li><Link to="#">Goods Receiving Note</Link></li>
+                                <li><Link to="#">Cash Purchase Invoice</Link></li>
+                                <li><Link to="#">Purchase Invoice</Link></li>
+                                <li><Link to="#">Purchase Return</Link></li>
+                                <li><Link to="#">Store Issuance</Link></li>
+                            </ul>
+                        </div>
             </li>
             <li>
-                <Button className='w-100'>
-                    <span className='icon'><MdAddchart /></span>
-                    Chart Of Accounts
-                    <span className='arrow'><FaAngleRight /></span>
-                </Button>
+                <Link to="/">
+                    <Button className='w-100'>
+                        <span className='icon'><MdAddchart /></span>
+                        Chart Of Accounts
+                        <span className='arrow'><FaAngleRight /></span>
+                    </Button>
+                </Link>   
             </li>
             <li>
-                <Button className='w-100'>
-                    <span className='icon'><MdOutlinePayments /></span>
-                    Account Vouchers
-                    <span className='arrow'><FaAngleRight /></span>
-                </Button>
+                <Link to="/">
+                    <Button className='w-100'>
+                        <span className='icon'><MdOutlinePayments /></span>
+                        Account Vouchers
+                        <span className='arrow'><FaAngleRight /></span>
+                    </Button>
+                </Link>
             </li>
             <li>
-                <Button className='w-100'>
-                    <span className='icon'><GiHumanPyramid /></span>
-                    Human Source
-                    <span className='arrow'><FaAngleRight /></span>
-                </Button>
+                <Link to="/">
+                    <Button className='w-100'>
+                        <span className='icon'><GiHumanPyramid /></span>
+                        Human Source
+                        <span className='arrow'><FaAngleRight /></span>
+                    </Button>
+                </Link>   
+
             </li>
             <li>
-                <Button className='w-100'>
-                    <span className='icon'><MdOutlineInventory2 /></span>
-                    Inventory
-                    <span className='arrow'><FaAngleRight /></span>
-                </Button>
+                <Link to="/">
+                    <Button className='w-100'>
+                        <span className='icon'><MdOutlineInventory2 /></span>
+                        Inventory
+                        <span className='arrow'><FaAngleRight /></span>
+                    </Button>
+                </Link> 
             </li>
             <li>
-                <Button className='w-100'>
-                    <span className='icon'><AttachMoneyOutlinedIcon/></span>
-                    Sale Order
-                    <span className='arrow'><FaAngleRight /></span>
-                </Button>
+                <Link to="/">
+                        <Button className='w-100'>
+                            <span className='icon'><AttachMoneyOutlinedIcon/></span>
+                            Sale Order
+                            <span className='arrow'><FaAngleRight /></span>
+                        </Button>
+                </Link>       
             </li>
             <li>
-                <Button className='w-100'>
-                    <span className='icon'><TbReportSearch /></span>
-                    Reports
-                    <span className='arrow'><FaAngleRight /></span>
-                </Button>
+                <Link to="/">
+                    <Button className='w-100'>
+                        <span className='icon'><TbReportSearch /></span>
+                        Reports
+                        <span className='arrow'><FaAngleRight /></span>
+                    </Button>
+                </Link>   
             </li>
             <li>
-                <Button className='w-100'>
-                    <span className='icon'><IoSettingsOutline /></span>
-                    Setting
-                    <span className='arrow'><FaAngleRight /></span>
-                </Button>
+                <Link to="/">
+                    <Button className='w-100'>
+                        <span className='icon'><IoSettingsOutline /></span>
+                        Setting
+                        <span className='arrow'><FaAngleRight /></span>
+                    </Button>
+                </Link>                
             </li>
         </ul>
     </div>
