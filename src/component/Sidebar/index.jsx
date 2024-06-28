f87c2c7c96ec0e5e141dba44133d5932bfe1c638
MuhammadFaizan42 <m.faizan42.mf@gmail.com>
2024-06-29 02:37:05 +0500

    dropdown toggle

--- src/component/Sidebar/index.jsx
+++ src/component/Sidebar/index.jsx
@@ -1,5 +1,5 @@
 import { Button } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 import { RxDashboard } from "react-icons/rx";
 import { FaAngleRight } from "react-icons/fa6";
 import { BiPurchaseTagAlt } from "react-icons/bi";
@@ -11,21 +11,22 @@ import AttachMoneyOutlinedIcon from '@mui/icons-material/AttachMoneyOutlined';
 import { TbReportSearch } from "react-icons/tb";
 import { IoSettingsOutline } from "react-icons/io5";
 import { Link } from 'react-router-dom';
-<IoSettingsOutline />
-
-
-
-
 
 
 
 const Sidebar = () => {
-  return (
+    const [activeTab,setActiveTab]=useState(0);
+    const [isToggleSubmenu,setIsToggleSubmenu]=useState(false);
+    const isOpenSubmenu=(index)=>{
+        setActiveTab(index)
+        setIsToggleSubmenu(!isToggleSubmenu)
+    }
+    return (
     <div className="sidebar">
         <ul>
             <li>
                 <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===0 ? 'active' : ''}`}>
                             <span className='icon'><RxDashboard /></span>
                             Dashboard
                             <span className='arrow'><FaAngleRight /></span>
@@ -34,12 +35,12 @@ const Sidebar = () => {
             </li>
             <li>
                 
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===1 && isToggleSubmenu===true ? 'active' : ''}`} onClick={()=>isOpenSubmenu(1)}>
                             <span className='icon'><BiPurchaseTagAlt /></span>
                             Purchase
                             <span className='arrow'><FaAngleRight /></span>
                         </Button>
-                        <div className='submenuWrapper'>
+                        <div className={`submenuWrapper ${activeTab===1 && isToggleSubmenu===true ? 'colapse' : 'colapsed'}`}>
                             <ul className='submenu'>
                                 <li><Link to="#">Purchase Order</Link></li>
                                 <li><Link to="#">Goods Receiving Note</Link></li>
@@ -52,7 +53,7 @@ const Sidebar = () => {
             </li>
             <li>
                 <Link to="/">
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab===2 ? 'active' : ''}`}>
                         <span className='icon'><MdAddchart /></span>
                         Chart Of Accounts
                         <span className='arrow'><FaAngleRight /></span>
@@ -61,7 +62,7 @@ const Sidebar = () => {
             </li>
             <li>
                 <Link to="/">
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab===3 ? 'active' : ''}`}>
                         <span className='icon'><MdOutlinePayments /></span>
                         Account Vouchers
                         <span className='arrow'><FaAngleRight /></span>
@@ -70,7 +71,7 @@ const Sidebar = () => {
             </li>
             <li>
                 <Link to="/">
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab===4 ? 'active' : ''}`}>
                         <span className='icon'><GiHumanPyramid /></span>
                         Human Source
                         <span className='arrow'><FaAngleRight /></span>
@@ -80,7 +81,7 @@ const Sidebar = () => {
             </li>
             <li>
                 <Link to="/">
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab===5 ? 'active' : ''}`}>
                         <span className='icon'><MdOutlineInventory2 /></span>
                         Inventory
                         <span className='arrow'><FaAngleRight /></span>
@@ -89,7 +90,7 @@ const Sidebar = () => {
             </li>
             <li>
                 <Link to="/">
-                        <Button className='w-100'>
+                        <Button className={`w-100 ${activeTab===6 ? 'active' : ''}`}>
                             <span className='icon'><AttachMoneyOutlinedIcon/></span>
                             Sale Order
                             <span className='arrow'><FaAngleRight /></span>
@@ -98,7 +99,7 @@ const Sidebar = () => {
             </li>
             <li>
                 <Link to="/">
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab===7 ? 'active' : ''}`}>
                         <span className='icon'><TbReportSearch /></span>
                         Reports
                         <span className='arrow'><FaAngleRight /></span>
@@ -107,7 +108,7 @@ const Sidebar = () => {
             </li>
             <li>
                 <Link to="/">
-                    <Button className='w-100'>
+                    <Button className={`w-100 ${activeTab===8 ? 'active' : ''}`}>
                         <span className='icon'><IoSettingsOutline /></span>
                         Setting
                         <span className='arrow'><FaAngleRight /></span>
